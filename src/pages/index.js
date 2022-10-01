e949eb98c1e917931275f1b25138f3a0674c912e
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-10-01 23:03:47 +0200

    UI changes to index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Layout } from "../components/Layout";
 import Card from "../components/Card";
 import Logo from "../components/Logo";
 
+import bgImage from "../images/musikquiz-1-cover.webp";
+
 export const query = graphql`
   query {
     allPrismicQuiz {
@@ -32,17 +34,24 @@ const IndexPage = ({ data }) => {
 
   return (
     <Layout>
-      <main className="bg-black">
-        <div className="flex flex-col min-h-screen max-w-screen-lg mx-auto">
-          <header className="pt-24 pb-4 mb-8 border-b-2 border-white">
+      <main className="bg-black min-h-screen">
+        <header
+          className="border-b-2 border-white bg-center"
+          style={{ backgroundImage: `url(${bgImage})` }}
+        >
+          <div className="flex flex-col max-w-screen-lg mx-auto pt-36 pb-56">
             <h1 className="font-sans-condensed font-extrabold text-4xl text-white">
               <span className="sr-only">Musikquiz</span>
-              <span className="w-32 block"><Logo fillColor="#FFF"/></span>
+              <span className="w-32 block">
+                <Logo fillColor="#FFF" />
+              </span>
             </h1>
-          </header>
-          <div className="grid grid-cols-2 gap-8 md:grid-cols-3">
-            {quizzes.map(quiz => {
-              return <Card key={`quiz-${quiz.node.uid}`} data={quiz} />
+          </div>
+        </header>
+        <div className="flex flex-col max-w-screen-lg mx-auto">
+          <div className="grid grid-cols-2 gap-8 md:grid-cols-3 -mt-20">
+            {quizzes.map((quiz) => {
+              return <Card key={`quiz-${quiz.node.uid}`} data={quiz} />;
             })}
           </div>
         </div>
