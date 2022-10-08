4568b1e4e379a0f89a26ab87d502d0552835796e
Daniel Tängerfors <daniel.tangerfor@gmail.com>
2022-10-08 21:24:02 +0200

    Moved pagination and added hotkeys feature

--- src/pages/quiz/{PrismicQuiz.uid}/answers.js
+++ src/pages/quiz/{PrismicQuiz.uid}/answers.js
@@ -1,11 +1,13 @@
-import React, {useEffect} from "react";
+import React, { useEffect } from "react";
 import { graphql } from "gatsby";
 import { useSelector, useDispatch } from "react-redux";
+import { useHotkeys } from "react-hotkeys-hook";
 import { Layout } from "../../../components/Layout";
 import Logo from "../../../components/Logo";
 
 import bgImage from "../../../images/musikquiz-1-cover.webp";
 import IndexButton from "../../../components/answers/IndexButton";
+import AnswerPagination from "../../../components/answers/AnswerPagination";
 
 export const answerQuery = graphql`
   query ($id: String) {
@@ -35,6 +37,11 @@ export const answerQuery = graphql`
         quiz_title {
           text
         }
+        video_background {
+          type
+          url
+          raw
+        }
       }
     }
   }
@@ -45,12 +52,15 @@ const AnswerPage = ({ data }) => {
 
   const dispatch = useDispatch();
   const questionIndex = useSelector((state) => state.index);
+  const questions = useSelector((state) => state.questions);
+
   const setQuestions = (value) => {
     dispatch({
       type: "SET_QUESTIONS",
       questions: value,
     });
   };
+
   const setThemeColor = (value) => {
     dispatch({
       type: "SET_THEME_COLOR",
@@ -58,32 +68,62 @@ const AnswerPage = ({ data }) => {
     });
   };
 
-  useEffect(() => {
-    dispatch({
-      type: "SET_INDEX",
-      index: 0
-    })
-  }, [])
+  useHotkeys(
+    "right",
+    () => {
+      if (!(questionIndex >= questions.length - 1)) {
+        dispatch({
+          type: "INCREMENT",
+          currentIndex: questionIndex,
+        });
+        dispatch({
+          type: "SET_QUIZ_COMPONTENT",
+          quiz_component: "song",
+        });
+      } else {
+        return null;
+      }
+    },
+    [questionIndex]
+  );
+
+  useHotkeys(
+    "left",
+    () => {
+      if (questionIndex >= 1) {
+        dispatch({
+          type: "DECREMENT",
+          currentIndex: questionIndex,
+        });
+        dispatch({
+          type: "SET_QUIZ_COMPONTENT",
+          quiz_component: "song",
+        });
+      }
+    },
+    [questionIndex]
+  );
+
+  // useEffect(() => {
+  //   dispatch({
+  //     type: "SET_INDEX",
+  //     index: 0,
+  //   });
+  // });
 
   setQuestions(quiz.body);
   setThemeColor(quiz.theme_color);
 
   return (
     <Layout>
-      <main style={{ backgroundImage: `url(${bgImage})` }}>
+      <main>
         <div className="flex flex-col min-h-screen justify-between max-w-screen-lg mx-auto">
           <header className="w-32 mt-12">
             <Logo fillColor={quiz.theme_color} />
           </header>
           <div className="bg-white border-2 border-black flex flex-col my-auto">
             <div className="border-b-2 border-black">
-              <ul className="flex">
-                {quiz.body.map((question, index) => {
-                  return (
-                    <IndexButton index={index}/>
-                  );
-                })}
-              </ul>
+              <AnswerPagination quiz={quiz.body} />
             </div>
             <div className="flex border-b-2 border-black">
               <div className="px-10 py-6 border-r-2 border-black flex-1">
@@ -124,6 +164,26 @@ const AnswerPage = ({ data }) => {
             </div>
           </div>
         </div>
+        {quiz.video_background.raw.kind === "image" ? (
+          <figure className="absolute -z-10 w-full h-full inset-0">
+            <img
+              src={quiz.video_background.url}
+              className="w-full h-full object-cover"
+            />
+          </figure>
+        ) : (
+          <figure className="absolute -z-10 w-full h-full inset-0">
+            <video
+              autoPlay
+              muted
+              loop
+              playsInline
+              className="w-full h-full object-cover"
+            >
+              <source src={quiz.video_background.url}></source>
+            </video>
+          </figure>
+        )}
       </main>
     </Layout>
   );
